bdaf3a6244152141c1cbdd1f955424f7170ad0c7
Goutham Kishore Krishnamoorthy <gouthamkishore.k@gmail.com>
2025-03-08 09:02:42 +0000

    resumetry3

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
-import Resumetry from "./components/Resume/ResumeTry";
+import Resumetry from "./components/resumetry/ResumeTry";
 
 import {
   BrowserRouter as Router,
