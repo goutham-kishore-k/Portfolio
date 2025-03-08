38fffe6ccbeb4ebb154d78db14848c1e3541a677
Goutham Kishore Krishnamoorthy <gouthamkishore.k@gmail.com>
2025-03-08 09:01:51 +0000

    ResumeTry2

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
+import Resumetry from "./components/Resume/ResumeTry";
+
 import {
   BrowserRouter as Router,
   Route,
@@ -39,6 +41,8 @@ function App() {
           <Route path="/project" element={<Projects />} />
           <Route path="/about" element={<About />} />
           <Route path="/resume" element={<Resume />} />
+          <Route path="/resumetry" element={<Resumetry />} />
+
           <Route path="*" element={<Navigate to="/"/>} />
         </Routes>
         <Footer />
